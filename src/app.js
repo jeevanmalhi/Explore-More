45018cae2cea128531b40f0ee1dfab2081699ec5
Jeevan <jeevan@Piyushs-MacBook-Air.local>
2019-05-03 22:28:22 -0400

    setup application for heroku

--- src/app.js
+++ src/app.js
@@ -81,7 +81,7 @@ app.get('*',(req,res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
 
-    console.log('Server is up on port 3000')
+    console.log('Server is up on port'+ port)
 })
